cbda1bd836fcabc34fa50885bfc1b33e2eb347ab
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-08-05 18:57:19 +0100

    Add UserAction

--- src/user/userAction.js
+++ src/user/userAction.js
@@ -1,21 +1,21 @@
-// import { FETCH_URL_REQUEST, FETCH_URL_SUCCESS, FETCH_URL_ERROR } from "./userType";
+import { FETCH_URL_REQUEST, FETCH_URL_SUCCESS, FETCH_URL_ERROR } from "./userType";
 
-// export const fetchUserRequest = () => {
-//     return {
-//         type: FETCH_URL_REQUEST
-//     }
-// }
+export const fetchUserRequest = () => {
+    return {
+        type: FETCH_URL_REQUEST
+    }
+}
 
-// export const fetchUserSuccess = (users) => {
-//     return {
-//         type: FETCH_URL_SUCCESS,
-//         payload: users
-//     }
-// }
+export const fetchUserSuccess = (users) => {
+    return {
+        type: FETCH_URL_SUCCESS,
+        payload: users
+    }
+}
 
-// export const fetchUserError = (error) => {
-//     return {
-//         type: FETCH_URL_ERROR,
-//         payload: error
-//     }
-// }
+export const fetchUserError = (error) => {
+    return {
+        type: FETCH_URL_ERROR,
+        payload: error
+    }
+}
